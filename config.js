17a9ddd19b6b4dba8e762daa28a4083efa2a48dc
tipoboludoeh-crypto <tipoboludoeh@gmail.com>
2026-02-06 13:39:16 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+    bot: {
+        owner: '593978619941@s.whatsapp.net',
+        timeouts: {
+            download: 20000,
+            send: 15000
+        }
+    },
+    mode: 'silent'
+};
